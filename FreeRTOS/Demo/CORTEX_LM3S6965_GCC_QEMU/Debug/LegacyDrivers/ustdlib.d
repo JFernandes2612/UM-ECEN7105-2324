76a933964d4bb2b9bc8fd055804624b504cead92
JFernandes2612 <businessdojoel@gmail.com>
2023-11-27 22:46:06 +0800

    empty bitch

--- FreeRTOS/Demo/CORTEX_LM3S6965_GCC_QEMU/Debug/LegacyDrivers/ustdlib.d
+++
@@ -1,4 +0,0 @@
-LegacyDrivers/ustdlib.o: \
- C:/FreeRTOSv202212.01/FreeRTOS/Demo/Common/drivers/LuminaryMicro/ustdlib.c \
- C:/FreeRTOSv202212.01/FreeRTOS/Demo/Common/drivers/LuminaryMicro/debug.h
-C:/FreeRTOSv202212.01/FreeRTOS/Demo/Common/drivers/LuminaryMicro/debug.h:
